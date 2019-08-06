0d13c11ba77a9b583a7e61bc515e767c00c91ddd
jjellis <jjjr1234@aol.com>
2019-08-05 20:51:05 -0500

    added migrations

---
+++ bookapi/bookapi/Migrations/20190806014845_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace bookapi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(maxLength: 250, nullable: false),
+                    Author = table.Column<string>(nullable: false),
+                    Category = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "books",
+                columns: new[] { "Id", "Author", "Category", "Title" },
+                values: new object[] { 1, " stien beck", "none", "The Grapes of Wrath" });
+
+            migrationBuilder.InsertData(
+                table: "books",
+                columns: new[] { "Id", "Author", "Category", "Title" },
+                values: new object[] { 2, "stien beck", "blue", "Cannery Row" });
+
+            migrationBuilder.InsertData(
+                table: "books",
+                columns: new[] { "Id", "Author", "Category", "Title" },
+                values: new object[] { 3, "stephen king", "boo", "The Shining" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "books");
+        }
+    }
+}
